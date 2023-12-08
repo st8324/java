e2b80ff21705b790504cd5cfec65e8d4ba72ec37
st8324 <stajun@naver.com>
2023-12-08 17:42:06 +0900

    자바 1일차 : 연습 예제

---
+++ java_jik/src/day01/TestEx1.java
@@ -0,0 +1,11 @@
+package day01;
+
+public class TestEx1 {
+
+	public static void main(String[] args) {
+		// 문제1.
+		// char ch1 = "A"; 수정
+		char ch1 = 'A';
+	}
+
+}
---
+++ java_jik/src/day01/TestEx2.java
@@ -0,0 +1,15 @@
+package day01;
+
+public class TestEx2 {
+
+	public static void main(String[] args) {
+		// 문제2. 국어, 영어, 수학 성적을 입력하여 평균을 구하려 한다. 
+		// 이 때 필요한 변수들을 선언하세요.
+		// 국어, 영어, 수학 점수를 어떤 값을 이용할 것인가? 점수를 정수로 할것인가? 실수로 할 것인가?
+		int kor1, eng1, math1, sum1;
+		double kor2, eng2, math2, sum2;
+		double avg;
+		
+	}
+
+}
