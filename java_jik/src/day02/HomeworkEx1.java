55b3fa0f539327e551d3fb06c6e5bf1ce5cca418
st8324 <stajun@naver.com>
2023-12-11 17:25:14 +0900

    자바 2일차 : 과제 - 성적 예제(문제만)

---
+++ java_jik/src/day02/HomeworkEx1.java
@@ -0,0 +1,17 @@
+package day02;
+
+public class HomeworkEx1 {
+
+	public static void main(String[] args) {
+		/* 성적을 입력받아 성적에 맞는 학점을 출력하는 코드를 작성하세요.
+		 * 90이상 ~ 100이하 : A
+		 * 80이상 ~ 90미만 : B
+		 * 70이상 ~ 80미만 : C
+		 * 60이상 ~ 70미만 : D
+		 * 60미만 : F
+		 * 0보다 작거나 100보다 큰 경우 : 잘못된 점수
+		 * */
+
+	}
+
+}
