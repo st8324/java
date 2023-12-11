1fba8111689ac1de4753a278ab7991a1eee6c63b
st8324 <stajun@naver.com>
2023-12-11 12:45:57 +0900

    자바 2일차 : 비교 연산자 예제

---
+++ java_jik/src/day02/RelationalOperatorEx1.java
@@ -0,0 +1,17 @@
+package day02;
+
+public class RelationalOperatorEx1 {
+	
+	//비교(관계)연산자 예제
+	public static void main(String[] args) {
+		int num1 = 1, num2 = 2;
+		
+		System.out.println(num1 + " > " + num2 + "  : " + (num1 > num2));
+		System.out.println(num1 + " >= " + num2 + " : " + (num1 >= num2));
+		System.out.println(num1 + " < " + num2 + "  : " + (num1 < num2));
+		System.out.println(num1 + " <= " + num2 + " : " + (num1 <= num2));
+		System.out.println(num1 + " == " + num2 + " : " + (num1 == num2));
+		System.out.println(num1 + " != " + num2 + " : " + (num1 != num2));
+	}
+
+}
