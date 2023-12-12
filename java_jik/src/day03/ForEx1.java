0085490ae134d66b0d123d8751b3b6e876e897e5
st8324 <stajun@naver.com>
2023-12-12 17:02:13 +0900

    자바 2일차 : for문 예제 - Hello world 출력

---
+++ java_jik/src/day03/ForEx1.java
@@ -0,0 +1,20 @@
+package day03;
+
+public class ForEx1 {
+
+	public static void main(String[] args) {
+		/* Hello world 5번 출력 예제 
+		 * 반복회수 	: i는 1부터 5까지 1씩 증가 
+		 * 규칙성		: Hello world를 출력
+		 * 반복회수는 초기화, 조건식, 증감식을 결정
+		 * 규칙성은 실행문을 결정
+		 * */
+		int i;
+		for( i = 1 ; i <= 5 ; ++i ) {
+			System.out.println("Hello world");
+		}
+		
+
+	}
+
+}
