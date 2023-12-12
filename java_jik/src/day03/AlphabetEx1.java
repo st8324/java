9350b2fddb2ebec41ad7578f79e5288ef54a67da
st8324 <stajun@naver.com>
2023-12-12 17:03:44 +0900

    자바 2일차 : for문 예제 - 알파벳 출력

---
+++ java_jik/src/day03/AlphabetEx1.java
@@ -0,0 +1,16 @@
+package day03;
+
+public class AlphabetEx1 {
+
+	public static void main(String[] args) {
+		/* a부터 z까지 반복문을 이용하여 출력하는 코드를 작성하세요.
+		 * 반복회수	: ch는 a부터 z까지 1씩 증가
+		 * 규칙성		: ch를 출력
+		 * */
+		
+		for(char ch = 'a'; ch <= 'z'; ++ch) {
+			System.out.print(ch);
+		}
+	}
+
+}
