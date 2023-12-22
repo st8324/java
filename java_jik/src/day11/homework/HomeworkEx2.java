053b6f330b5121796e3e4cbf9e688bbd8e9f9b6d
st8324 <stajun@naver.com>
2023-12-22 17:12:12 +0900

    자바 11일차 : 과제

---
+++ java_jik/src/day11/homework/HomeworkEx2.java
@@ -0,0 +1,12 @@
+package day11.homework;
+
+public class HomeworkEx2 {
+
+	/* 두 정수와 산술 연산자를 입력받아 결과를 출력하는 코드를 작성하세요.
+	 * 메서드를 생성
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ java_jik/src/day11/homework/HomeworkEx3.java
@@ -0,0 +1,15 @@
+package day11.homework;
+
+public class HomeworkEx3 {
+
+	public static void main(String[] args) {
+		/* 사이트에서 회원 관리(로그인, 회원가입 등)를 위한 Member 클래스를 작성하세요.*/
+		
+		//작성한 Member 클래스를 이용하여 인스턴스를 생성하고 구현한 메서드를 테스트해보세요.
+		
+	}
+
+}
+class Member{
+	//필요한 멤버변수 및 메서드를 정리하고 구현하세요.
+}
