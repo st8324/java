ef2505d359110794c31907f3ea7e7cf3a8b42774
st8324 <stajun@naver.com>
2024-01-12 17:27:50 +0900

    자바 과제 : 대학생 관리 프로그램 - 틀 생성

---
+++ java_jik/src/university/vo/Major.java
@@ -0,0 +1,8 @@
+package university.vo;
+
+/**학생이 소속된 전공을 나타내는 클래스
+ * - 대학생 정보에 필요하다고 생각되는 정보를 멤버 변수로 추가해보세요.
+ * */
+public class Major {
+
+}
---
+++ java_jik/src/university/vo/Student.java
@@ -0,0 +1,9 @@
+package university.vo;
+
+
+/**대학생 정보를 나타내는 클래스
+ * - 대학생 정보에 필요하다고 생각되는 정보를 멤버 변수로 추가해보세요. 
+ * */
+public class Student {
+
+}
