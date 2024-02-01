93695c6a4497a76d6e19323e2b9b9eea14684537
st8324 <stajun@naver.com>
2024-02-01 10:52:00 +0900

    DB 2일차 : DDL 예제

---
+++ db/sql/1_DDL 예제.sql
@@ -0,0 +1,33 @@
+/* 여러줄 주석
+*/
+# 데이터베이스 추가(없으면)
+create database if not exists TEST;
+# create schema if not exists `TEST`;
+
+# 데이터베이스 삭제(있으면)
+# drop database if exists TEST;
+# drop schema if exists `TEST`;
+
+USE TEST;
+
+# 테이블 삭제
+DROP TABLE IF EXISTS MEMBER;
+
+# 테이블 생성
+CREATE TABLE IF NOT EXISTS MEMBER(
+	ID VARCHAR(13) PRIMARY KEY,
+    PW VARCHAR(15) NOT NULL,
+    EMAIL VARCHAR(30) NOT NULL UNIQUE
+);
+DESC MEMBER;
+DROP TABLE IF EXISTS BOARD;
+CREATE TABLE IF NOT EXISTS BOARD(
+	NUM INT AUTO_INCREMENT,
+    TITLE VARCHAR(50) NOT NULL,
+    CONTENT LONGTEXT NOT NULL,
+    VIEW INT NOT NULL DEFAULT 0,
+    ID VARCHAR(13) NOT NULL,
+    PRIMARY KEY(NUM),
+    FOREIGN KEY(ID) REFERENCES MEMBER(ID)
+);
+DESC BOARD;
