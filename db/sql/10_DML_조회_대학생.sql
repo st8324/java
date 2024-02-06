e3a859873397d5c68d63401df9a76044b8fe44a2
st8324 <stajun@naver.com>
2024-02-06 16:25:43 +0900

    DB 5일차 : 조회 복습

--- db/sql/10_DML_조회_대학생.sql
+++ db/sql/10_DML_조회_대학생.sql
@@ -38,3 +38,81 @@ FROM
         JOIN
     LECTURE ON CO_LE_NUM = LE_NUM
 GROUP BY LE_NUM;
+
+
+# 컴퓨터공학을 다니는 학생 정보를 조회
+select * from student where st_major = '컴퓨터공학';
+
+# 컴퓨터공학을 다니는 1학년 학생 정보를 조회 
+select * from student where st_major = '컴퓨터공학' and st_grade = 1;
+
+# 컴퓨터공학 또는 화학공학을 다니는 1학년 학생 정보를 조회
+select * from student where (st_major = '컴퓨터공학' or st_major = '화학공학') and st_grade = 1;
+select * from student where st_major in('컴퓨터공학', '화학공학') and st_grade = 1;
+
+# 컴퓨터공학을 다니는 학생 수를 조회, group by, count()
+select count(*) from student where st_major = '컴퓨터공학' group by st_major;
+
+# 학생들이 등록된 학과를 조회 
+select distinct st_major from student;
+
+# 학생들이 3명이상 등록된 학과들을 조회 
+select st_major from student group by st_major having count(st_num) >= 3;
+
+# 컴퓨터개론을 수강하는 학생 수를 조회
+SELECT 
+    count(*)
+FROM
+    course
+        JOIN
+    lecture ON co_le_num = le_num
+WHERE
+    le_title = '컴퓨터개론'
+group by co_le_num;
+
+# 각 강의별 수강하는 학생수를 조회, 강의명과 학생수를 조회 
+SELECT 
+    le_title, COUNT(co_st_num)
+FROM
+    course
+        JOIN
+    lecture ON co_le_num = le_num
+GROUP BY co_le_num;
+
+# 홍길동 학생이 수강하는 강의목록을 조회 
+SELECT 
+    st_name, le_title
+FROM
+    course
+        JOIN
+    student ON co_st_num = st_num
+		JOIN
+	lecture ON co_le_num = le_num
+WHERE
+    st_name = '홍길동';
+    
+# 기계공학에 소속된 교수의 수를 조회 
+select count(*) from professor where pr_major = '기계공학';
+
+# 김교수가 강의하는 강의명을 조회 
+SELECT DISTINCT
+    pr_name, le_title
+FROM
+    course
+        JOIN
+    lecture ON co_le_num = le_num
+        JOIN
+    professor ON le_pr_num = pr_num
+WHERE
+    pr_name = '김교수';
+
+
+
+
+
+
+
+
+
+
+
--- db/sql/8_DML_조회 쇼핑몰.sql
+++ db/sql/8_DML_조회 쇼핑몰.sql
@@ -122,6 +122,69 @@ WHERE
 	OR_STATE IS NULL OR OR_STATE NOT IN("환불", "반품")
 GROUP BY PR_CODE;
 
+# 모든 제품을 조회하는 쿼리 
+select * from product;
+
+# 모든 카테고리를 조회하는 쿼리 
+select * from category;
+
+# 제품별 카테고리를 조회. 카테고리, 제품을 조회 
+SELECT 
+    ca_name as 카테고리, pr_title as 제품명
+FROM
+    product
+        JOIN
+    category ON ca_num = pr_ca_num;
+
+# 기타 카데고리에 포함된 모든 제품을 조회 
+SELECT 
+    ca_name, product.*
+FROM
+    product
+        JOIN
+    category ON ca_num = pr_ca_num
+WHERE
+    ca_name = '기타';
+
+# abc123회원이 주문한 제품 목록을 조회 
+SELECT 
+    or_date, or_state, or_amount, or_total_price, pr_title
+FROM
+    `order`
+        JOIN
+    product ON pr_code = or_pr_code
+WHERE
+    or_me_id = 'abc123';
+
+# 제품별 판매수량을 조회
+SELECT 
+    product.*, IFNULL(SUM(or_amount), 0) AS '판매수량'
+FROM
+    `order`
+        RIGHT JOIN
+    product ON pr_code = or_pr_code
+WHERE or_state NOT IN('반품','환불') OR or_state IS NULL
+GROUP BY pr_code;
+
+# 인기 제품 조회. 인기 제품은 누적 판매량을 기준으로 상위 5개 제품 
+SELECT 
+    product.*, IFNULL(SUM(or_amount), 0) AS '판매수량'
+FROM
+    `order`
+        RIGHT JOIN
+    product ON pr_code = or_pr_code
+WHERE or_state NOT IN('반품','환불') OR or_state IS NULL
+GROUP BY pr_code
+ORDER BY 판매수량 DESC, pr_price ASC
+LIMIT 0, 5;
+
+# 가격이 제일 비싼 제품을 조회 
+SELECT 
+    *
+FROM
+    product
+ORDER BY pr_price DESC
+LIMIT 0 , 1;
 
 
 
