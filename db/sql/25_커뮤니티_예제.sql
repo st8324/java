07afa75e087b95c5518dd7d52d7cb9af82030552
st8324 <stajun@naver.com>
2024-02-14 16:09:50 +0900

    DB 8일차 : 커뮤니티 - 상황 예제

--- db/sql/25_커뮤니티_예제.sql
+++ db/sql/25_커뮤니티_예제.sql
@@ -96,9 +96,10 @@ CREATE PROCEDURE BOARD_RECOMMEND(
 BEGIN
 	DECLARE _RE_NUM INT;
     DECLARE _RE_STATE INT;
+    DECLARE _NEW_STATE INT;
     # 1. 아이디, 게시글 번호를 이용하여 등록된 추천 번호를 조회해서 변수에 저장
     SET _RE_NUM = 
-		(SELECT * FROM RECOMMEND WHERE RE_ME_ID = _ID AND RE_BO_NUM = _BO_NUM);
+		(SELECT RE_NUM FROM RECOMMEND WHERE RE_ME_ID = _ID AND RE_BO_NUM = _BO_NUM);
     # 2-1. 추천 번호가 NULL이면 추천 테이블에 추가 
     IF _RE_NUM IS NULL THEN
 		INSERT INTO RECOMMEND(RE_ME_ID, RE_BO_NUM, RE_STATE)
@@ -108,22 +109,32 @@ BEGIN
     ELSE
 		# 2-2-0. 추천 번호에 맞는 추천 상태를 가져옴 
 		SET _RE_STATE = (SELECT RE_STATE FROM RECOMMEND WHERE RE_NUM = _RE_NUM);
+        /*
         # 2-2-1. 추천 상태가 _STATE와 같으면 취소(0) => 0으로 수정  
         IF _RE_STATE = _STATE THEN
 			UPDATE RECOMMEND SET RE_STATE = 0 
             WHERE RE_BO_NUM = _BO_NUM AND RE_ME_ID = _ID;
 		
-        # 2-2-1. 추천 상태가 _STATE와 다르면 _STATE로 수정 
+        # 2-2-2. 추천 상태가 _STATE와 다르면 _STATE로 수정 
         ELSE
 			UPDATE RECOMMEND SET RE_STATE = _STATE 
             WHERE RE_BO_NUM = _BO_NUM AND RE_ME_ID = _ID;
         END IF;
+        */
+        # 2-2-1. 추천 상태가 _STATE와 같으면 취소(0) => 0으로 수정  
+        IF _RE_STATE = _STATE THEN
+			SET _NEW_STATE = 0;
+        # 2-2-2. 추천 상태가 _STATE와 다르면 _STATE로 수정 
+        ELSE
+			SET _NEW_STATE = _STATE;
+        END IF;
+        UPDATE RECOMMEND SET RE_STATE = _NEW_STATE 
+            WHERE RE_BO_NUM = _BO_NUM AND RE_ME_ID = _ID;
 	END IF;
 END //
 DELIMITER ;
 
-CALL BOARD_RECOMMEND('abc123', 1, 1);
-
+CALL BOARD_RECOMMEND('abc123', 1, -1);
 
 
 
