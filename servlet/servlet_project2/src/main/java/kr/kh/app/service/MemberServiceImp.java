2641a074862e6e25aac2ab8483584e08fe34ade4
st8324 <stajun@naver.com>
2024-02-26 12:43:06 +0900

    서블릿 3일차 : 서블릿 복습 - 아이디 중복 예외처리 적용

--- servlet/servlet_project2/src/main/java/kr/kh/app/service/MemberServiceImp.java
+++ servlet/servlet_project2/src/main/java/kr/kh/app/service/MemberServiceImp.java
@@ -41,8 +41,14 @@ public class MemberServiceImp implements MemberService {
 		//아이디 중복체크 
 		
 		//각 항목 유효성 검사
-		
-		boolean res = memberDao.insertMember(member);
-		return res;
+
+		//아래 예외 처리는 아이디 중복체크 검사를 안했기 때문에 발생하는 예외를 임시 처리하기 위한 방법
+		try {
+			boolean res = memberDao.insertMember(member);
+			return res;
+		}catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 }
