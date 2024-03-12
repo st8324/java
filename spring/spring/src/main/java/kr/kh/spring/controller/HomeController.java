2c0f014aa08d78d5df753dfcf514e2e78afd8e2d
st8324 <stajun@naver.com>
2024-03-12 11:49:32 +0900

    스프링 1일차 : 데이터 전송 예제(서버 <->화면)

--- spring/spring/src/main/java/kr/kh/spring/controller/HomeController.java
+++ spring/spring/src/main/java/kr/kh/spring/controller/HomeController.java
@@ -4,8 +4,11 @@ import java.util.Locale;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import kr.kh.spring.model.dto.TestDTO;
 
 @Controller
 public class HomeController {
@@ -13,8 +16,23 @@ public class HomeController {
 	//value : url, method : 전송박식을 GET 또는 POST
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Model model) {
-		
+		//model.addAttribute("화면에서 사용할 이름","보낼 데이터");
+		model.addAttribute("name","홍길동");
 		return "home";
 	}
 	
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public String homePost(Model model,TestDTO testDto/*,String name, int age*/) {
+		/*System.out.println("이름 : " + name);
+		System.out.println("나이 : " + age);
+		TestDTO testDto = new TestDTO(name, age);
+		*/
+		System.out.println(testDto);
+		return "home";
+	}
+	@RequestMapping(value = "/test/{num}", method = RequestMethod.GET)
+	public String test(Model model,@PathVariable("num")int num) {
+		System.out.println("경로 데이터 : " + num);
+		return "redirect:/";
+	}
 }
--- spring/spring/src/main/webapp/WEB-INF/views/home.jsp
+++ spring/spring/src/main/webapp/WEB-INF/views/home.jsp
@@ -10,6 +10,15 @@
 <h1>
 	Hello world!  
 </h1>
-<P>  The time on the server is ${serverTime}. </P>
+<P>  안녕하세요. 제 이름은 ${name} 입니다.</P>
+<form action="<c:url value="/"/>" method="post">
+	<input type="text" name="name" placeholder="이름"> 
+	<br>
+	<input type="number" name="age" placeholder="나이">
+	<br>
+	<button type="submit">전송</button>
+</form>
+<!-- url 경로에 데이터를 추가하여 서버에 전송  -->
+<a href="<c:url value="/test/123"/>">링크 테스트</a>
 </body>
 </html>
