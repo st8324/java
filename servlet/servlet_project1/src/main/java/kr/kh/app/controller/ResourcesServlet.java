c7b8968c8859c3308bfd6063d41b397d577eb3f3
st8324 <stajun@naver.com>
2024-03-15 09:20:23 +0900

    서블릿 : 정적파일 접근 예제

--- servlet/servlet_project1/src/main/java/kr/kh/app/controller/ResourcesServlet.java
+++ servlet/servlet_project1/src/main/java/kr/kh/app/controller/ResourcesServlet.java
@@ -19,12 +19,15 @@ public class ResourcesServlet extends HttpServlet {
 	private String uploadPath = "/resources";
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		//URL에서 컨택스트 패스와 /resources를 제거하는 작업
+		//  /servlet_project1/resources/css/test.css
+		//  => /css/test.css
 		String path = request.getRequestURI().replaceFirst("/resources", "");
 		path = path.replaceFirst(request.getContextPath(), "");
+		
         String fileName = path;
         String filePath = getServletContext().getRealPath(uploadPath) + fileName;
-        System.out.println(getServletContext().getRealPath(uploadPath));
+
         File file = new File(filePath);
         try(FileInputStream fis = new FileInputStream(file);
             OutputStream os = response.getOutputStream()){
