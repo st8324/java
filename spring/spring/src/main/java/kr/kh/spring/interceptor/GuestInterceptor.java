bd11f591c6e00225747d5b452213d9b07878b2bc
st8324 <stajun@naver.com>
2024-03-18 14:46:43 +0900

    스프링 4일차 : GuestInterceptor 추가 및 적용

---
+++ spring/spring/src/main/java/kr/kh/spring/interceptor/GuestInterceptor.java
@@ -0,0 +1,32 @@
+package kr.kh.spring.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import kr.kh.spring.model.vo.MemberVO;
+
+public class GuestInterceptor extends HandlerInterceptorAdapter {
+
+	/* preHandle에서 return값이 true이면 가려던 컨트롤러로 가서 작업을 진행
+	 * return값이 false이면 가려던 컨트롤러로 가지 마세요. 리다이렉트할 경로가 있으면 해당 경로로 이동
+	 * */
+	@Override
+	public boolean preHandle(
+		HttpServletRequest request, 
+		HttpServletResponse response, 
+		Object handler)
+	    throws Exception {
+		MemberVO user = (MemberVO) request.getSession().getAttribute("user");
+		//로그인 했으면
+		if(user != null) {
+			response.sendRedirect(request.getContextPath() + "/");
+			return false;
+		}
+		
+		return true;
+	}
+
+}
