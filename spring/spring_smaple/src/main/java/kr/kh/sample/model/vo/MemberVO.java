4e5bbaa5f70be1f02ea9b39001cb2d5b880e53f1
st8324 <stajun@naver.com>
2024-04-09 16:39:55 +0900

    스프링 샘플 : sse를 이용한 채팅 예제 - MemberVO 추가

---
+++ spring/spring_smaple/src/main/java/kr/kh/sample/model/vo/MemberVO.java
@@ -0,0 +1,22 @@
+package kr.kh.sample.model.vo;
+
+import java.util.Date;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class MemberVO {
+
+	private String me_id;
+	private String me_pw;
+	private String me_ms_state; 
+	private String me_email; 
+	private String me_authority; 
+	private String me_stop;
+	private int me_fail;
+	private String me_cookie;
+	private Date me_cookie_limit;
+	private boolean autoLogin;
+}
